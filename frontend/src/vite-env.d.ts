14868bf8ce8703afe257419ad3f0e79a5c58c3a3
Leonardo Santos <leonardo.santos@plannix.com.br>
2025-06-06 10:51:17 -0300

    Fix Build and Seeders

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="vite/client" />
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
+declare module '*.jpeg' {
+  const value: any;
+  export default value;
+}
+declare module '*.gif' {
+  const value: any;
+  export default value;
+}
+declare module '*.svg' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}

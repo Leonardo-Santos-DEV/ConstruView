16340ef15c8ea8efa9a64dd2fba7dcc22e97f570
Leonardo Santos <leonardo.santos@plannix.com.br>
2025-06-07 10:08:17 -0300

    Fix Bug Permission

--- backend/src/helpers/hasPermission.ts
+++ backend/src/helpers/hasPermission.ts
@@ -1,12 +1,11 @@
 import Permission from '../models/Permission';
 
 export async function hasPermission(userId: number, projectId: number, requiredLevel: number): Promise<boolean> {
-  console.log(userId, projectId, requiredLevel);
   let permission: Permission | null;
-  if (!projectId) {
-    permission = await Permission.findOne({where: {userId}});
-  } else {
+  if (projectId) {
     permission = await Permission.findOne({where: {userId, projectId}});
+  } else {
+    permission = await Permission.findOne({where: {userId}});
     if (!permission) {
       return true;
     }

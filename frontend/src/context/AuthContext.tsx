1ee9a0575a1c0aa6ad97e58dc048aaeacb5b0120
Leonardo Santos <leonardo.santos@plannix.com.br>
2025-06-09 10:42:58 -0300

    Debug auth

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -21,6 +21,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({children}) => {
       setIsLoading(true);
       try {
         const currentUser = await apiCheckAuthStatus();
+        console.log('2. [AuthContext] Resposta da API (apiCheckAuthStatus):', currentUser);
         if (currentUser) {
           setUser(currentUser);
         } else {

e620effd0d55c7b7efff6d6da0f27c975af9df13
Leonardo Santos <leonardo.santos@plannix.com.br>
2025-06-13 04:22:08 -0300

    Changed to postgreesql

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,14 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS, {
-  dialect: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
 });
 
 export default sequelize;

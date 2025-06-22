da2a31345dd4717d189a6d434885776d1eacf18b
Leonardo Santos <leonardo.santos@plannix.com.br>
2025-06-21 21:00:11 -0300

    FIz Error CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ import cors from 'cors';
 const app = express();
 
 const corsOptions = {
-  origin: /^(http:\/\/localhost:\d+|https:\/\/.*\.onrender\.com|https:\/\/construview\.ai)$/,
+  origin: /^(http:\/\/localhost:\d+|https:\/\/.*\.onrender\.com|https:\/\/construview\.ai|https:\/\/.*\.leonardosantos\.dev)$/,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,

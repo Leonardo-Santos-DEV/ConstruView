e2401ddceee26f7b91bd7fca8946cf6784c138e2
Leonardo Santos - Software Developer <contato@leonardosantos.dev>
2025-07-22 01:19:03 -0300

    Configuration

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -6,12 +6,6 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
 });
 
 export default sequelize;
